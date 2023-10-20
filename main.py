2d3dedd6ff1571b054a7af88d1bbb8f7ae4c0daf
guimaraes-lucas <luc53706@gmail.com>
2023-10-20 18:59:38 -0300

    First Commit

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 # Pong Simples em PYTHON 3 para Iniciantes
-# Por @TokyoEdTech
 
 # Parte 1: Iniciando
 
 import pygame
 
+# Configuração Inicial
 pygame.init()
-window = pygame.display.set_mode((800, 600))
 clock = pygame.time.Clock()
+
+# Configuração da tela principal
+largura_tela = 1280
+altura_tela = 960
+window = pygame.display.set_mode((largura_tela, altura_tela))
+pygame.display.set_caption('Pong')
+
+# Objetos do Jogo
+bola = pygame.Rect(largura_tela / 2 - 15, altura_tela / 2 - 15, 30, 30)
+jogador = pygame.Rect(largura_tela - 20, altura_tela / 2 - 70, 10, 140)
+oponente = pygame.Rect(10, largura_tela / 2 - 70, 10, 140)
+
+cor_fundo = pygame.color('grey12')
+cinza_claro = (200, 200, 200)
 running = True
 
 while running:
