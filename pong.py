41ef919cdaf94235503ab46c3cd47652fccc1109
guimaraes-lucas <luc53706@gmail.com>
2023-10-21 09:39:12 -0300

    Update

--- pong.py
+++ pong.py
@@ -1,8 +1,5 @@
-# Pong Simples em PYTHON 3 para Iniciantes
+import pygame, sys, random
 
-# Parte 1: Iniciando
-
-import pygame, sys
 
 def animacao_bola():
     global velocidade_bola_x, velocidade_bola_y
@@ -14,31 +11,40 @@ def animacao_bola():
     if bola.top <= 0 or bola.bottom >= altura_tela:
         velocidade_bola_y *= -1
     elif bola.left <= 0 or bola.right >= comprimento_tela:
-        velocidade_bola_x *= -1
+        reinicia_bola()
 
     if bola.colliderect(jogador) or bola.colliderect(oponente):
         velocidade_bola_x *= -1
 
+
 def animacao_jogador():
     # Movimento do Jogador
     jogador.y += velocidade_jogador
+
     if jogador.top <= 0:
         jogador.top = 0
     elif jogador.bottom >= altura_tela:
         jogador.bottom = altura_tela
 
+
 def animacao_oponente():
     # Movimento do Oponente
     if oponente.top < bola.y:
-        oponente.top += velocidade_oponente
+        oponente.y += velocidade_oponente
     elif oponente.bottom > bola.y:
-        oponente.bottom -= bola.y
+        oponente.y -= velocidade_oponente
 
     if oponente.top <= 0:
         oponente.top = 0
     elif oponente.bottom >= altura_tela:
         oponente.bottom = altura_tela
 
+
+def reinicia_bola():
+    global velocidade_bola_y, velocidade_bola_x
+    bola.center = (comprimento_tela/2, altura_tela/2)
+    velocidade_bola_y *= random.choice((1, -1))
+    velocidade_bola_x *= random.choice((1, -1))
 
 # Configuração Inicial
 pygame.init()
@@ -51,18 +57,18 @@ window = pygame.display.set_mode((comprimento_tela, altura_tela))
 pygame.display.set_caption('Pong')
 
 # Objetos do Jogo
-bola = pygame.Rect(comprimento_tela/2 - 15, altura_tela/2 - 15, 30, 30)
-oponente = pygame.Rect(comprimento_tela - 20, altura_tela/2 - 70, 10, 140)
-jogador = pygame.Rect(10, altura_tela/2 - 70, 10, 140)
+bola = pygame.Rect(comprimento_tela / 2 - 15, altura_tela / 2 - 15, 30, 30)
+oponente = pygame.Rect(comprimento_tela - 20, altura_tela / 2 - 70, 10, 140)
+jogador = pygame.Rect(10, altura_tela / 2 - 70, 10, 140)
 
 cor_fundo = pygame.Color('grey12')
 cinza_claro = (200, 200, 200)
 
 # Velocidade dos Objetos
-velocidade_bola_x = 7
-velocidade_bola_y = 7
+velocidade_bola_x = 7 * random.choice((1, -1))
+velocidade_bola_y = 7 * random.choice((1, -1))
 velocidade_jogador = 0
-velocidade_oponente = 7
+velocidade_oponente = 6
 
 while True:
     for event in pygame.event.get():
@@ -71,15 +77,15 @@ while True:
             sys.exit()
 
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_w:
+            if event.key == pygame.K_w or event.key == pygame.K_UP:
                 velocidade_jogador -= 6
-            if event.key == pygame.K_s:
+            if event.key == pygame.K_s or event.key == pygame.K_DOWN:
                 velocidade_jogador += 6
 
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_w:
+            if event.key == pygame.K_w or event.key == pygame.K_UP:
                 velocidade_jogador += 6
-            if event.key == pygame.K_s:
+            if event.key == pygame.K_s or event.key == pygame.K_DOWN:
                 velocidade_jogador -= 6
 
     animacao_bola()
@@ -91,7 +97,7 @@ while True:
     pygame.draw.rect(window, cinza_claro, jogador)
     pygame.draw.rect(window, cinza_claro, oponente)
     pygame.draw.ellipse(window, cinza_claro, bola)
-    pygame.draw.aaline(window, cinza_claro, (comprimento_tela/2, 0), (comprimento_tela/2, altura_tela))
+    pygame.draw.aaline(window, cinza_claro, (comprimento_tela / 2, 0), (comprimento_tela / 2, altura_tela))
 
     pygame.display.flip()
     clock.tick(60)
